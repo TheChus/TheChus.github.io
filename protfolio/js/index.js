239ae57c8ace7ec45409a60c80f11d5b1f524e81
TheChus <63579215+TheChus@users.noreply.github.com>
2020-06-09 06:33:14 +0800

    更新天氣小卡

--- protfolio/js/index.js
+++ protfolio/js/index.js
@@ -40,28 +40,19 @@ $.ajax({
         var color = " "
         if (PoP >= 0 && PoP < 30) {
             img_src = "./img/icons/sun.png";
-            advice = "快樂出門"
         } else if (PoP >= 30 && PoP < 60) {
             img_src = "./img/icons/cloud.png";
-            advice = "帶個雨傘"
         } else {
             img_src = "./img/icons/rain.png";
-            advice = "攜帶雨具"
         }
 
         content.innerHTML += `
             <div class="ajaxcard">
                 <img src="${img_src}" alt="">
                 <h4>${city}</h4>
-                <h5>今明36小時天氣預報：</h5>
                 <h5>${Wx}</h5>
-                <h5>氣溫${MinT}~${MaxT}℃</h5>
-                <h5>您需要帶傘的機率 <span id="size">${PoP}%</span></h5>
-                <div class="test"><h5>建議您：</h5>
-                    <div>
-                        <span id="go">${advice}</span>
-                    </div>
-                </div>
+                <h5><span>↓</span>${MinT}°&ensp; <span>↑</span>${MaxT}°</h5>
+                <h5 id="lasth5">降雨機率 <span id="size">${PoP}%</span></h5>
             </div>
         `
     }
